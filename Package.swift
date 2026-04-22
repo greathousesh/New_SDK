31460429ff23ccc31eb96c8eec8340162becbfbc
fw1088 <fw1088@163.com>
2026-04-21 17:48:54 -0700

    initial: setup binary distribution for v0.0.24

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MySDK",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "MySDK",
+            targets: ["MySDK"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MySDK",
+            url: "https://github.com/greathousesh/iOS_SDK/releases/download/v0.0.24/MySDK.xcframework.zip",
+            checksum: "7a1eafe6ddaea6ec7c815ab171fa56dd53c593ddebdf3c176296fb40c6b76028"
+        )
+    ]
+)
